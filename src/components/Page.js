b411f836618f164e857a3e778e33a97f5d6ef9dd
PaulsBecks <paul.beck.berlin@gmail.com>
2020-02-26 21:48:05 +0100

    added bulid and new features

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Page = ({children, singleMode, id}) => (<div
-  id={id} className="bg-white shadow-1 center pa4" 
-  style={{width: "210mm", height: singleMode ? "297mm" : ""}}
->
-  {children}
-</div>); 
+const Page = ({ children, singleMode, id }) => {
+  return (
+    <div
+      id={id}
+      className="bg-white shadow-1 center pa4"
+      style={{ width: "175mm", height: singleMode ? "297mm" : "" }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default Page;
--- src/components/SinglePage.js
+++ src/components/SinglePage.js
@@ -2,20 +2,25 @@ import React from "react";
 import Page from "./Page";
 import "./SinglePage.css";
 const SinglePage = ({
+  id,
   invoice: {
-    id,
-    order_date,
-    send_date,
-    customer: { name, ust, discount },
+    order_date = "24.02.2020",
+    send_date = "24.02.2020",
+    invoice_date = "24.02.2020",
+    invoice_number = "142787164221",
+    customer: { name, ust, discount, address, city, postCode },
     articles = [],
-    invoice_date,
-    invoice_number,
     send_default_price = 5.0
   }
 }) => {
   const articles_net_price = articles
     .filter(a => !a.empty)
-    .map(a => a.price)
+    .map(({ price, amount }) => {
+      const totalPrice = price * amount;
+      const totalPriceWithDiscount = totalPrice - totalPrice * (discount / 100);
+      const net = totalPriceWithDiscount / (1 + ust / 100);
+      return net;
+    })
     .reduce((total, x) => parseFloat(x) + total, 0);
 
   return (
@@ -25,111 +30,164 @@ const SinglePage = ({
         <div className="invoice-header-company-address"></div>
       </div>
       <div className="invoice-body">
-        <div className="invoice-subject">
-          <h2>Rechnung</h2>
-          <p>Wir erlauben uns in Rechnung zu stellen:</p>
-          <div className="invoice-body-order-date">
-            <p>
-              <b>Bestelldatum: {order_date}</b>
-            </p>
+        <div className="invoice-body-top">
+          <div className="invoice-subject">
+            <h2>Rechnung</h2>
+            <p>Wir erlauben uns in Rechnung zu stellen:</p>
+            <div className="invoice-body-subject-key-values">
+              <div className="invoice-body-subject-keys">
+                <div className="invoice-body-order-date">
+                  <p>
+                    <b>Bestelldatum:</b>
+                  </p>
+                </div>
+                <div className="invoice-body-send-date">
+                  <p>
+                    <b>Versand am:</b>
+                  </p>
+                </div>
+                <div className="invoice-body-send-to">
+                  <p>
+                    <b>Versand an:</b>
+                  </p>
+                </div>
+              </div>
+              <div className="invoice-body-subject-values">
+                <div className="invoice-body-order-date">
+                  <p>
+                    <b>{order_date}</b>
+                  </p>
+                </div>
+                <div className="invoice-body-send-date">
+                  <p>
+                    <b>{send_date}</b>
+                  </p>
+                </div>
+                <div className="invoice-body-send-to">
+                  <p>
+                    <b>
+                      {name} • {}
+                    </b>
+                  </p>
+                  <p>
+                    <b>
+                      {address} • {postCode} {city}
+                    </b>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="invoice-body-send-date">
-            <p>
-              <b>Versand am: {send_date}</b>
-            </p>
-          </div>
-          <div className="invoice-body-send-to">
-            <p>
-              <b>Versand am: {name}</b>
-            </p>
-          </div>
-        </div>
-        <div>
-          <div className="invoice-body-invoice-date">
-            <p>
-              Rechnungsdatum: <b>{invoice_date}</b>
-            </p>
-          </div>
-          <div className="invoice-body-invoice-nr">
-            <p>
-              Rechnungsnummer: <b>{invoice_number}</b>
-            </p>
+          <div>
+            <div className="invoice-body-invoice-date">
+              <p>
+                Rechnungsdatum: <b>{invoice_date}</b>
+              </p>
+            </div>
+            <div className="invoice-body-invoice-nr">
+              <p>
+                Rechnungsnummer: <b>{invoice_number}</b>
+              </p>
+            </div>
+            <div className="invoice-body-invoice-hint">
+              <p>(Bitte bei Zahlungen angeben)</p>
+            </div>
           </div>
         </div>
         <div className="invoice-body-article-wrapper">
           {articles
             .filter(a => !a.empty)
-            .map(article => (
-              <div className="invoice-body-article">
-                <div className="invoice-body-article-left">
-                  <div className="invoice-body-article-description">
-                    <div>
-                      <b>
-                        {article.amount} Exemplar
-                        {parseInt(article.amount) > 1 && "e"}
-                      </b>
+            .map(({ amount, price, name, isbn }) => {
+              const multiple = parseInt(amount) > 1;
+              const totalPrice = price * amount;
+              const totalPriceWithDiscount =
+                totalPrice - totalPrice * (discount / 100);
+              const net = totalPriceWithDiscount / (1 + ust / 100);
+              return (
+                <div className="invoice-body-article">
+                  <div className="invoice-body-article-left">
+                    <div className="invoice-body-article-description">
+                      <div>
+                        <b>
+                          {amount} Exemplar
+                          {multiple && "e"}
+                        </b>
+                      </div>
+                      <div className="invoice-body-article-title">
+                        <b>„{name}“</b>
+                      </div>
+                      <div className="invoice-body-article-title">{isbn}</div>
                     </div>
-                    <div className="invoice-body-article-title">
-                      <b>„{article.name}“</b>
-                    </div>
-                    <div className="invoice-body-article-title">
-                      {article.isbn}
-                    </div>
+                    <div className="invoice-body-artivle-price-calc">{`Preis ${
+                      multiple ? "je" : ""
+                    } ${price} €${
+                      multiple ? ` = ${totalPrice.toFixed(2)}€` : ""
+                    }${
+                      discount > 0
+                        ? `, abzüglich ${discount} % Rabatt = ${totalPriceWithDiscount.toFixed(
+                            2
+                          )} €`
+                        : ""
+                    } (beinhaltet ${ust} % MwST = ${(
+                      totalPriceWithDiscount - net
+                    ).toFixed(2)})`}</div>
                   </div>
-                  <div className="invoice-body-artivle-price-calc">{`Preis ${
-                    article.price
-                  } €${
-                    discount > 0
-                      ? `, abzüglich ${discount} % Rabat = ${(
-                          article.price -
-                          (article.price * discount) / 100
-                        ).toFixed(2)} €`
-                      : ""
-                  } (beinhaltet ${ust} % MwST = ${(
-                    article.price -
-                    article.price * (discount / 100) -
-                    (article.price - article.price * (discount / 100)) /
-                      (1 + ust / 100)
-                  ).toFixed(2)})`}</div>
+                  <div className="invoice-body-article-price">
+                    <b>{net.toFixed(2)}€</b>
+                  </div>
                 </div>
-                <div className="invoice-body-article-price">
-                  {article.price}€
-                </div>
-              </div>
-            ))}
+              );
+            })}
         </div>
         <div className="invoice-body-price-calculation">
           <div>
             <p>Netto</p>
-            <p>{articles_net_price.toFixed(2)}€</p>
+            <p>
+              <b>{articles_net_price.toFixed(2)}€</b>
+            </p>
           </div>
           <div>
             <p>Versandpauschale</p>
-            <p>{send_default_price.toFixed(2)}€</p>
+            <p>
+              <b>{send_default_price.toFixed(2)}€</b>
+            </p>
           </div>
           <div>
             <p>{`+${ust}% Mehrwertsteuer`}</p>
             <p>
-              {(
-                ((articles_net_price + send_default_price) * parseFloat(ust)) /
-                100
-              ).toFixed(2)}
-              €
+              <b>
+                {(
+                  ((articles_net_price + send_default_price) *
+                    parseFloat(ust)) /
+                  100
+                ).toFixed(2)}
+                €
+              </b>
             </p>
           </div>
         </div>
         <div className="invoice-body-price">
           <p>Rechnungsbetrag</p>
-          <p>{(articles_net_price + send_default_price * 1.07).toFixed(2)}€</p>
+          <p>
+            <b>
+              {(
+                (articles_net_price + send_default_price) *
+                (1 + parseFloat(ust) / 100)
+              ).toFixed(2)}
+              €
+            </b>
+          </p>
         </div>
         <div>
           <p>
             Zahlen Sie bitte den Rechnungsbetrag bis zum 16. März 2020 unter
-            Angabe der Rechnungsnummer auf unten stehendes Konto. <br /> Die
-            gelieferte Ware bleibt bis zur vollständigen Bezahlung Eigentum des
-            Findling Verlages. <br />
-            Vielen Dank für Ihre Bestellung. Mit herzlichen Grüßen, Findling
-            Verlag. Werneuchen, den 24. Februar 2020
+            Angabe der Rechnungsnummer auf unten stehendes Konto. <br />
+            <br /> Die gelieferte Ware bleibt bis zur vollständigen Bezahlung
+            Eigentum des Test Verlages. <br />
+            Vielen Dank für Ihre Bestellung.
+            <br />
+            <br /> Mit herzlichen Grüßen, Test Verlag.
+            <br /> Werneuchen, den 24. Februar 2020
           </p>
         </div>
       </div>
