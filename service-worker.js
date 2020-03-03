da0a8b818297ef236693197718d1cacc2c57ed18
PaulsBecks <paul.beck.berlin@gmail.com>
2020-03-03 20:00:58 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/invoice-from-form/precache-manifest.8cf659618a799f49f9ad64168203f5bd.js"
+  "/invoice-from-form/precache-manifest.d4703296fc89f0c9d675e82f3b255ee7.js"
 );
 
 self.addEventListener('message', (event) => {
