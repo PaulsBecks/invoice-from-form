ef93b71d49202667cfffeaeb934303da824c7ca0
PaulsBecks <paul.beck.berlin@gmail.com>
2020-07-08 10:29:31 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.941e58cbbd4519c756f47b7d6db0df81.js"
+  "/precache-manifest.5b0aeeb413930f339de5a73005bd0218.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/10.79693cfc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-4798tj"]=this["webpackJsonpreact-4798tj"]||[]).push([[10],{826:function(e,t,a){"use strict";a.r(t);var n=a(15),c=a(0),r=a.n(c),u=a(31),l=a(268),s=a(265);function i(){var e=Object(u.j)(),t=Object(n.a)(e,1)[0];return t&&t.user&&!t.user.placeholder?r.a.createElement("div",{className:"invoice-app-container"},r.a.createElement(l.a,null)):r.a.createElement(s.a,null)}a.d(t,"default",(function(){return i}))}}]);
-//# sourceMappingURL=10.79693cfc.chunk.js.map
