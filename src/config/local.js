9258e9a03b8cbbf5d7e815a21fbde664387eb3b1
PaulsBecks <paul.beck.berlin@gmail.com>
2020-07-17 17:59:42 +0200

    added production plans

--- src/config/local.js
+++ src/config/local.js
@@ -1,7 +1,5 @@
 export default {
   backendURL: "http://localhost:8000",
-  stripeKey:
-    "pk_test_51H5ZfXJ8wibMbP4F9HgqOVWjAsn4JAayev8UaTOvr0nt6TUZTf4rlZMiL9JZvoBD1aojBXJbsYptCU6ZigmmpMRT00Aqf6zaiI",
   paypalPlanIds: {
     monthly: {
       basic: "P-9PA15792TK449582DL4IY3TQ",
--- src/config/production.js
+++ src/config/production.js
@@ -1,3 +1,13 @@
 export default {
   backendURL: "https://api.billeroo.de",
+  paypalPlanIds: {
+    monthly: {
+      basic: "P-9PA15792TK449582DL4IY3TQ",
+      premium: "P-67781698V4327152TL4IY4JQ",
+    },
+    yearly: {
+      basic: "P-91H76602RW429845DL4IY37Y",
+      premium: "P-54436245XD620721NL4IY4UY",
+    },
+  },
 };
